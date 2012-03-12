28c7bccf5d7714a4b6ab7c0f0a4c636894118bf1
rebecca <rebecca.mui@colostate.edu>
2012-03-12 11:06:23 -0600

    reworked ui, reads chainfile and puts data in awgetRequest ready to hook up to FileRetrieverService

--- src/awget/awget.cc
+++ src/awget/awget.cc
@@ -10,101 +10,165 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <netdb.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
 
 #include "../common/protocol.h"
+#include "../common/stepping_stone.h"
 
+void showUsageandExit()
+{
+	printf("\nUsage:  awget <URL> [-c chainfile] \n");
+	exit(-1);
+}
 
 int main(int argc, char** argv)
 {
-
-	// check args
-	char *chainFileName = NULL;
-	char *fileURL = NULL;
-	char *defaultChainFileName = "chaingang.txt";
+	char* chainFileName = NULL;
+	char* fileURL = NULL;
+	char defaultChainFileName[] = "chaingang.txt";
 
 
 	// == parse command line arguments ==
-	int option;
-	bool invalidArgs = false;
-    int index;
+	if (argc != 2 && argc != 4)
+		showUsageandExit();
 
-	while ((option = getopt (argc, argv, "c:")) != -1)
-		switch (option)
-        {
-           case 'c':
-        	   chainFileName = optarg;
-        	   break;
-
-           case '?':
-             if (optopt == 'c' )
-               printf ("Option -%c requires an argument.\n", optopt);
-             else
-            	 invalidArgs = true;
-
-          default:
-             abort ();
-        }
-
-		if (invalidArgs || optind < 0 || optind > 1)
+	for (int i=1; i<argc; i++)
+	{
+		if ( strcmp(argv[i], "-c") == 0 )
 		{
-			printf("\nUsage:  awget <URL> [-c chainfile] \n");
-			return 1;
+			chainFileName = argv[i+1];
+			if (chainFileName == NULL)
+				showUsageandExit();
+			i++;
 		}
+		else if (argv[i][0]== '-')
+			showUsageandExit();
+		else
+			fileURL = argv[i];
+	}
 
-		fileURL = argv[optind];
-
-		// this for debug
-	    printf ("URL = %s, chainfile = %s\n", fileURL, (chainFileName==NULL?"none":chainFileName));
-
-
-	    // The chainfile argument is optional, and specifies the file containing information about
-	    // the chain you want to use.  Since the chainfile argument is optional, if not specified
-	    // awget should read the chain configuration from a local file called chaingang.txt.
-	    // If no chainfile is given at the command line and awget fails to locate the chaingang.txt file,
-	    // awget should print an error message and exit.
+	// this for debug
+	printf ("URL = %s, chainfile = %s\n", fileURL, chainFileName);
 
 
-	    if (chainFileName != NULL)
-	    {
-	    	// check to make sure file exists
-
-	    	// if so, read file
-
-		    // if not or error reading, print error message and exit
-	    }
-	    else
-	    {
-	    	// check to see if default chain file exists
-	    	// if so, read file
-
-		    // if not or error reading, print error message and exit
-	    }
+	// The chainfile argument is optional, and specifies the file containing information about
+	// the chain you want to use.  Since the chainfile argument is optional, if not specified
+	// awget should read the chain configuration from a local file called chaingang.txt.
+	// If no chainfile is given at the command line and awget fails to locate the chaingang.txt file,
+	// awget should print an error message and exit.
 
 
+	if (chainFileName == NULL)
+		chainFileName = defaultChainFileName;
+
+    ifstream chainFile(chainFileName);
+
+    if (!chainFile.is_open())
+    {
+    	printf("Error reading chainfile: %s  (File does not exist)\n", chainFileName);
+    	return -1;
+    }
+
+    string data;
+	string address;
+	int port;
+
+	AwgetRequest awgetRequest;
+//	awgetRequest.url = NULL;
+
+
+	// read number of entries
+	getline(chainFile, data);
+	int numEntries = atoi(data.c_str());
+
+	if (numEntries <= 0)
+	{
+		printf("Error reading chainfile: %s  (Invalid format)\n", chainFileName);
+		return -1;
+	}
+
+	awgetRequest.chainListSize = numEntries;
+
+	/*
+ 	if (numEntries > MAX_HOSTS)
+	{
+		printf("Error reading chainfile: %s  (Max # entries exceeded)\n", chainFileName);
+		return -1;
+	}
+	*/
+
+	try
+	{
+		for (int i=0; i<numEntries; i++)
+		{
+			if (getline(chainFile, address, ','))
+			{
+				if (address.empty())
+				{
+					printf("Error reading chainfile: %s  (Invalid address)\n", chainFileName);
+					return -1;
+				}
+			}
+			else
+			{
+				printf("Error reading chainfile: %s \n", chainFileName);
+				return -1;
+			}
+
+			if (getline(chainFile, data))
+			{
+				port = atoi(data.c_str());
+
+				if (port < 1024 || port > 65535)
+				{
+					printf("Error reading chainfile: %s  (Invalid port)\n", chainFileName);
+					return -1;
+				}
+			}
+			else
+			{
+				printf("Error reading chainfile: %s \n", chainFileName);
+				return -1;
+			}
+
+			// copy data to struct
+			ssAddress entry;
+			strcpy(entry.hostAddress,address.c_str());
+			entry.port = port;
+			awgetRequest.chainList[i] = entry;
+		}
+	}
+	catch (...)
+	{
+		printf("Error reading chainfile: %s  (Invalid port)\n", chainFileName);
+		return -1;
+	}
+
+	// for testing - verify file read
+	printf("%s has %i entries\n", chainFileName, awgetRequest.chainListSize);
+	for (int i=0; i< awgetRequest.chainListSize; i++)
+		printf("%s, %i\n", awgetRequest.chainList[i].hostAddress, awgetRequest.chainList[i].port);
 
 
 
-	    // pick a random SS from the file to contact next.
+	// === set up FileRetrieverService and send request ===
+
+//	FileRetrieverService service;
+//	int socketID = 6000;
+//	service.handleRequest(&awgetRequest, socketID);
 
 
+	// === set up listener and wait for the data to arrive
 
 
-	    // connect to SS and sends the URL and the chain list from the file, after stripping the selected entry.
-
-
-
-
-	    // wait for the data to arrive
-
-
-
-
-	    // save it into a local file - Note that the name of the file should be the one given in the URL (but not the entire URL).
-
+	// save it into a local file - Note that the name of the file should be the one given in the URL (but not the entire URL).
 
 
 	return 1;
 }
+
 
 
